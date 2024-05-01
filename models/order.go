dbbe737b7dc69bb046a67e91976029d98e20db6e
nurzzaat <210103336@stu.sdu.edu.kz>
2024-05-01 12:03:45 +0500

    update orderItem

--- models/order.go
+++ models/order.go
@@ -1,17 +1,18 @@
 package models
 
 type Order struct {
-    ID          uint    `gorm:"primaryKey"`
-    UserID      uint    `gorm:"not null"`
-    OrderTime   string  `gorm:"default:CURRENT_TIMESTAMP"`
-    Status      string  `gorm:"not null"`
-    TotalAmount float64 `gorm:"not null"`
+	ID          uint    `gorm:"primaryKey"`
+	UserID      uint    `gorm:"not null"`
+	OrderTime   string  `gorm:"default:CURRENT_TIMESTAMP"`
+	Status      string  `gorm:"not null"`
+	TotalAmount float64 `gorm:"not null"`
 }
 
 type OrderItem struct {
-    ID        uint    `gorm:"primaryKey"`
-    OrderID   uint    `gorm:"not null"`
-    ItemName  string  `gorm:"not null"`
-    Quantity  uint    `gorm:"not null"`
-    Price     float64 `gorm:"not null"`
+	ID       uint   `gorm:"primaryKey"`
+	OrderID  uint   `gorm:"not null"`
+	ItemName string `gorm:"not null"`
+	Quantity uint   `gorm:"not null"`
+	NotType  string
+	Price    float64 `gorm:"not null"`
 }
