6a914cfb4e4a7c6ad9b65f2e003a565367bd5352
ertai_b03@mail.ru <95402990+BEkasss11@users.noreply.github.com>
2024-05-01 12:48:53 +0500

    Change

---
+++ controllers/PaymentType.go
@@ -0,0 +1,25 @@
+package controllers 
+
+func GetAllPaymentTypes(c *gin.Context) {
+    // TODO: Implement logic to retrieve all payment types from the database
+}
+
+// CreatePaymentType creates a new payment type
+func CreatePaymentType(c *gin.Context) {
+    // TODO: Implement logic to create a new payment type in the database
+}
+
+// GetPaymentTypeByID retrieves a payment type by its ID
+func GetPaymentTypeByID(c *gin.Context) {
+    // TODO: Implement logic to retrieve a payment type by its ID from the database
+}
+
+// UpdatePaymentTypeByID updates a payment type by its ID
+func UpdatePaymentTypeByID(c *gin.Context) {
+    // TODO: Implement logic to update a payment type by its ID in the database
+}
+
+// DeletePaymentTypeByID deletes a payment type by its ID
+func DeletePaymentTypeByID(c *gin.Context) {
+    // TODO: Implement logic to delete a payment type by its ID from the database
+}
