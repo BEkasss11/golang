5f33cc4239fc88f63835fc2baf14533d690cc94d
ertai_b03@mail.ru <95402990+BEkasss11@users.noreply.github.com>
2024-05-01 12:33:20 +0500

    Changing Payment

---
+++ models/Payment.go
@@ -0,0 +1,12 @@
+package models
+
+type Payment struct {
+    ID               uint    `gorm:"primaryKey"`
+    OrderID          uint    `gorm:"not null"`
+    Amount           float64 `gorm:"not null"`
+    PaymentTime      string  `gorm:"default:CURRENT_TIMESTAMP"`
+    CardNumber       string  `gorm:"not null"`
+    CardExpiryMonth  int     `gorm:"not null"`
+    CardExpiryYear   int     `gorm:"not null"`
+    CardCVV          string  `gorm:"not null"`
+}
--- models/menu.go
+++ models/menu.go
@@ -9,13 +9,3 @@ type Menu struct {
 	LastUpdated  string  `gorm:"default:CURRENT_TIMESTAMP"`
 }
 
-type Payment struct {
-    ID               uint    `gorm:"primaryKey"`
-    OrderID          uint    `gorm:"not null"`
-    Amount           float64 `gorm:"not null"`
-    PaymentTime      string  `gorm:"default:CURRENT_TIMESTAMP"`
-    CardNumber       string  `gorm:"not null"`
-    CardExpiryMonth  int     `gorm:"not null"`
-    CardExpiryYear   int     `gorm:"not null"`
-    CardCVV          string  `gorm:"not null"`
-}
